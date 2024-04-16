7b66324d13eb3ea59ec290b33e8ba388a782e9c8
1234-star <1294517310@qq.com>
2024-04-17 01:10:59 +0800

    更新 Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -1375,6 +1375,20 @@ export default {
           })
     },
     getBackendVersion() {
+      this.$axios
+          .get(
+              this.form.customBackend + "/version"
+          )
+          .then(res => {
+            this.backendVersion = res.data.replace(/backend\n$/gm, "");
+            this.backendVersion = this.backendVersion.replace("subconverter", "SubConverter");
+            let a = this.form.customBackend.indexOf("sur.wfy.one") !== -1 || this.form.customBackend.indexOf("sur.wfy.one") !== -1;
+            let b = this.form.customBackend.indexOf("127.0.0.1") !== -1;
+            a ? this.$message.success(`${this.backendVersion}` + "肥羊负载均衡增强版后端，已屏蔽免费节点池（会返回403），额外支持vless reality+hysteria+hysteria2订阅转换") : b ? this.$message.success(`${this.backendVersion}` + "本地局域网自建版后端") : this.$message.success(`${this.backendVersion}` + "官方原版后端不支持vless/hysteria订阅转换");
+          })
+          .catch(() => {
+            this.$message.error("自建后端");
+          });
     }
   }
 };
